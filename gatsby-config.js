ede011b77d5b789fba5e8289ae88823957b99d77
Happy-2020 <babystar6666@outlook.com>
2020-12-21 02:32:55 +0900

    Initialize

---
+++ gatsby-config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby Demo Site`,
+    description: `An awesome post does have some cool description.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+      spaceId: `p5ef1xcxky1t`,
+      accessToken: `QUdpkKU53d6pX6uDdYpycZ6UkiMr0TunLsmhdXC6njI`
+      }
+    },
+    `gatsby-plugin-preact`
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title='Home' keywords={[`gatsby`, `application`, `react`]} />
+    <div className='home'>
+      <h1>Hello Ishan</h1>
+      <p>Welcome your awesome Blog</p>
+      <div>
+        <div
+          style={{
+            maxWidth: `340px`,
+            margin: '0 auto',
+          }}
+        >
+          <Image />
+        </div>
+      </div>
+      <Link to='/blogposts/'>View all posts</Link>
+    </div>
+  </Layout>
+);
+
+export default IndexPage;
